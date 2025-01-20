d62e53fc84283f716bb0cf63db9d547ece48bf17
Mazen Afifi <mazen.afifi@flairstech.com>
2025-01-20 15:21:05 +0200

    .

--- src/assets/icons/ArrowIconToTop.tsx
+++ src/assets/icons/ArrowIconToTop.tsx
@@ -2,6 +2,5 @@ const ArrowIconToTop: React.FC<React.SVGProps<SVGSVGElement>> = () => (
     <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M6.71463 19.3904L21.2103 8.53488M21.2103 8.53488L9.80223 6.89685M21.2103 8.53488L19.5722 19.9429" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
-
 );
 export default ArrowIconToTop;
