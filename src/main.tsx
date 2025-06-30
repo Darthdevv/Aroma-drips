bcab91dda00c94611ab2823fbc2d3f4fb411cebf
Mazen Youssef <148579017+MazenUcef@users.noreply.github.com>
2025-07-01 01:25:27 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import AccessibilitySettings from './components/AccessibilitySettings.tsx'
+// import AccessibilitySettings from './components/AccessibilitySettings.tsx'
 import { ThemeProvider } from './context/themeContext.tsx'
 import { Toaster } from 'react-hot-toast';
 
